2b8bcea45845df21de1239fe9697ebd3a8bb7d74
nbeekman <nathan.beekman1@gmail.com>
2013-07-24 17:49:21 -0600

    adding content to each project. added new home page

---
+++ new/js/home.js
@@ -0,0 +1,8 @@
+/* home page interaction */
+function fadeOut() {
+  document.getElementById("homeContent").className = "drop";
+}
+
+function fadeIn() {
+  document.getElementById("homeContent").className = "";
+}
