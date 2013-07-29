94038d235a4ccc8ad47b862c8c621cf75cd89ceb
nbeekman <nathan.beekman1@gmail.com>
2013-07-29 17:20:38 -0600

    pushed new site to production!

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,78 @@
+/* setting up the cookie so users don't have to seem animation past their first visit */
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
+
 /* two.js animation */
 $(function() {
+
+  //create cookie if none found
+  var cookieValue = readCookie('nathanLogo');
+  if(cookieValue != 'here'){
+    createCookie('nathanLogo', 'here', 1);
+  }
+  /*
+else{
+    console.log('cookie found. its value is "' + cookieValue + '"');
+    //eraseCookie('nathanLogo');
+  }
+*/
 
   var two = new Two({
     fullscreen: false
   }).appendTo(document.getElementById("nathan"));
 
-  $.get('img/iAmNathanOutline.svg', function(doc) {
+  $.get('/img/nathanOutline.svg', function(doc) {
 
     var fresh = two.interpret($(doc).find('svg')[0]);
-    var t = 0;
+    
+    //if cookie is present, don't do the animation
+    if(cookieValue == "here"){
+      var t = 1;
+    }else{
+      var t = 0;
+    }
     var startOver, movingmouse = false;
     var hasFinished = false;
     var clearT = function() {
-      t = 0;
+      //t = 0;
       setEnding(fresh, 0);
       startOver = _.after(60, clearT);
     };
     var stopMouse = _.debounce(function() {
       movingmouse = false;
     }, 1000);
+    
+    /* setting size of vector output */
+    two.width = 320;
+    two.height = 150;
 
     fresh.center().translation.set(two.width / 2, two.height / 2);
     fresh.distances = calculateDistances(fresh);
     fresh.total = 0;  fresh.stroke = 'white';
     fresh.linewidth = 1;
     fresh.fill = "white";
+    fresh.scale = 0.5;
     _.each(fresh.distances, function(d) {
       fresh.total += d;
     });
@@ -52,18 +100,17 @@ $(function() {
         })
         .bind('update', function() {
 
-          /*if (movingmouse) {
+          if (movingmouse) {
             return;
-          }*/
+          }
           if (hasFinished == false) {
           
             if (t < 0.9999) {
               t += 0.00625;
             } else {
               hasFinished = true;
-              /*startOver();*/
+              startOver();
             }
-          
             setEnding(fresh, t);
           }
         }).play();
@@ -74,10 +121,16 @@ $(function() {
       if (hasFinished == true) {
         var rect = fresh.getBoundingClientRect();
         movingmouse = true;
-        t = cmap(e.clientX-(window.innerWidth/2)+320, rect.left, rect.right, 0, 1);
+        t = cmap(e.clientX-(window.innerWidth/3.3)+320, rect.left, rect.right, 0, 1);
         setEnding(fresh, t);
         stopMouse();
+        setTimeout(finishAnimation, 5000);
       }
+    }
+    
+    function finishAnimation() {
+      hasFinished = false;
+      startOver();
     }
 
     function setEnding(group, t) {
@@ -128,6 +181,7 @@ function cmap(v, i1, i2, o1, o2) {
 }
 
 /* Mobile screen lock */
+/*
 var lockTouchScreen = function( isLocked ) {
   if( isLocked == false ) {
     document.ontouchmove = null;
@@ -137,14 +191,52 @@ var lockTouchScreen = function( isLocked ) {
     };
   }
 };
-
 lockTouchScreen( isLocked = true );
+*/
 
 /* fade in on text below logo */
 function addClass() {
-  var dev = document.getElementById("dev");
-  dev.className += " fadeIn";
-  //console.log("class added");
+  var descr = document.getElementById("nathanDescr");
+  
+  if(cookieValue == 'true'){
+    descr.className = "";
+  }else{
+    descr.className += " fadeIn";
+  }
+}
+if(cookieValue = 'true'){
+  addClass();
+}else{
+  setTimeout(addClass, 3000);
 }
 
-setTimeout(addClass, 3000);
+function loadClass() {
+  var content = document.getElementById("content");
+  content.className = "load"
+}
+loadClass();
+
+
+/* mobile menu */
+
+$('.accordion-item h2').click(function(e) {
+  
+  $('#content').addClass('increaseTop');
+  // Register the variables for better performance
+  var parent = $(this).closest('.accordion-item'),
+      content = parent.find('.content');
+
+  // If the clicked section is not the active one
+  if (!parent.hasClass('open')) {
+    // close menu and remove class
+    $('.accordion-item.current .content').parent().removeClass('open');
+    
+    // Add class and slide down content
+    parent.addClass('open');
+    $('.content').removeClass('hidden');
+  }else{
+    parent.removeClass('open');
+    $('#content').removeClass('increaseTop');
+    $('.content').addClass('hidden');
+  }
+});
