a91a3ed4b1f025a7b6b3f6f4d8558f0e1da70aa3
Nathan Beekman <nathan.beekman1@gmail.com>
2020-09-06 09:42:10 -0600

    Footer changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -17,7 +17,7 @@ const Logo = ({ logo, url, ...props }) => {
 const FooterLayout = ({ linkedIn, github, twitter }) => {
   return (
     <footer className="bg-near-black white">
-      <div className="mw9 center pv3 tr">
+      <div className="mw9 center pv3 tc">
         <Logo logo={github} url="https://github.com/nbeekman" />
         <Logo logo={linkedIn} url="https://linkedin.com/in/nathanbeekman/" />
         <Logo logo={twitter} url="https://twitter.com/nathanbeekman" />
