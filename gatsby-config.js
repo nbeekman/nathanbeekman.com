479ef17d83a7f1c569786726fdcf5c0fb2fbc102
nbeekman <2294672+nbeekman@users.noreply.github.com>
2023-01-03 21:50:47 -0700

    title change in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://nathanbeekman.com`,
-    siteTitle: `Nathan Beekman | Senior Frontend Engineer`,
+    siteTitle: `Nathan Beekman | Front-End Practice Lead`,
     siteDescription: `A personal portfolio site.`,
     twitter: `@nathanbeekman`,
     github: `@nbeekman`,
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `Nathan Beekman | Senior Frontend Engineer`,
+        name: `Nathan Beekman | Front-End Practice Lead`,
         short_name: `nathanbeekman.com`,
         start_url: `/`,
         display: `minimal-ui`,
