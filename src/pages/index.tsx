d325c746ce479a79c96ea69c5c8a12784544ec71
nbeekman <2294672+nbeekman@users.noreply.github.com>
2026-07-27 12:24:49 -0600

    adding Herdware

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ type DataProps = {
   allMdx: {
     nodes: {
       frontmatter: {
-        date: string;
+        lastUpdated: string;
         description: string;
         image: IGatsbyImageData;
         project: string;
@@ -51,11 +51,11 @@ export const query = graphql`
   query workQuery {
     allMdx(
       filter: {frontmatter: {category: {eq: "work"}}}
-      sort: [{frontmatter: {featuredClient: DESC}}, {frontmatter: {date: DESC}}, {frontmatter: {clientName: DESC}}]
+      sort: [{frontmatter: {featuredClient: DESC}}, {frontmatter: {lastUpdated: DESC}}, {frontmatter: {clientName: DESC}}]
     ) {
       nodes {
         frontmatter {
-          date
+          lastUpdated
           clientName
           url
           project
